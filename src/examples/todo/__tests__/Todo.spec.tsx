5f613e62efbf42c0ee0f177db26bce5c20c1d160
Matt Dunn <matt.j.dunn@gmail.com>
2020-10-25 11:21:20 +0000

    Update sample tests to show JSX usage

--- src/examples/todo/__tests__/Todo.spec.tsx
+++ src/examples/todo/__tests__/Todo.spec.tsx
@@ -5,16 +5,23 @@
  * @licence MIT
  */
 
+/** @jsx jsx **/
+
+import {jsx} from "packages/render";
 import {mount} from "test/utils";
 
 import {Todo} from "../Todo";
 
 describe("Todo: Example of using mount", () => {
   it("should render component", async () => {
+    const fixture = {id: "1", text:"My Todo Item", complete: true};
+
     const deleteTodo: any = jest.fn();
     const updateTodo: any = jest.fn();
 
-    const wrapper = mount(Todo({todo: {id: "1", text:"My Todo Item", complete: true}, deleteTodo, updateTodo}));
+    const wrapper = mount(
+      <Todo todo={fixture} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+    );
 
     expect(wrapper.getDOMNode()).toMatchSnapshot();
   });
