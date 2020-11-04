e7c1abc169dada5d9665edc6bc3304b4f79bb029
Matt Dunn <matt.j.dunn@gmail.com>
2020-11-04 18:53:27 +0000

    Cleanup useState

--- packages/render/hooks/useState.ts
+++ packages/render/hooks/useState.ts
@@ -17,27 +17,24 @@ type UseState<S> = {
 export const useState = <S = any>(initialState: S): [S, (newValue: S) => S] => {
   const hook = activeHooks.getCurrent<UseState<S>>();
 
-  let currentState = hook.getValue();
+  const currentState = hook.getValue();
 
-  if (currentState === undefined) {
-    currentState = hook.setValue({
-      type: HOOK_TYPE,
-      value: initialState,
-      setter(newValue: S) {
-        if (currentState && currentState.value !== newValue) {
-          currentState.value = newValue;
-          hook.setValue({
-            ...currentState,
-            value: newValue,
-          });
-          hook.options.render();
-        }
-        return newValue;
-      },
-    });
-  }
+  const updatedState = hook.setValue({
+    type: HOOK_TYPE,
+    value: currentState?.value ?? initialState,
+    setter(newValue: S) {
+      if (updatedState.value !== newValue) {
+        hook.setValue({
+          ...updatedState,
+          value: newValue,
+        });
+        hook.options.render();
+      }
+      return newValue;
+    },
+  });
 
-  return [currentState.value, currentState.setter];
+  return [updatedState.value, updatedState.setter];
 };
 
 useState.type = HOOK_TYPE;
